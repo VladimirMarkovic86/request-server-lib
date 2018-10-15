8f298aea24dfc3989744e1a23aff683a2a8b8b4b
VladimirMarkovic86 <markovic.vladimir86@gmail.com>
2018-10-15 17:47:25 +0200

    Prepared project for clojars.

--- project.clj
+++ project.clj
@@ -1,11 +1,15 @@
-(defproject org.vladimir/request-server-lib "0.1.0"
+(defproject org.clojars.vladimirmarkovic86/request-server-lib "0.1.0"
   :description "Request server library"
-  :url "http://gitlab:1610/VladimirMarkovic86/request-server-lib"
+  :url "http://github.com/VladimirMarkovic86/request-server-lib"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
-                 [org.vladimir/utils-lib "0.1.0"]
-                 [org.vladimir/ajax-lib "0.1.0"]
+                 [org.clojars.vladimirmarkovic86/utils-lib "0.1.0"]
+                 [org.clojars.vladimirmarkovic86/ajax-lib "0.1.0"]
                  ]
+
+  :min-lein-version "2.0.0"
+  
   :source-paths ["src/clj"]
   :main request-server-lib.core)
+
