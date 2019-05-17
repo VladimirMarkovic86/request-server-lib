9c0cfe40f5acb9581072a77441160ca154e5ee91
VladimirMarkovic86 <markovic.vladimir86@gmail.com>
2019-05-17 15:48:24 +0200

    Task 78: Implemented canvas library to convert charts from html to images, and pass them to pdf files.

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject org.clojars.vladimirmarkovic86/request-server-lib "0.1.2"
+(defproject org.clojars.vladimirmarkovic86/request-server-lib "0.1.3"
   :description "Request server library"
   :url "http://github.com/VladimirMarkovic86/request-server-lib"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
-                 [org.clojars.vladimirmarkovic86/utils-lib "0.4.8"]
+                 [org.clojars.vladimirmarkovic86/utils-lib "0.4.9"]
                  [org.clojars.vladimirmarkovic86/ajax-lib "0.1.9"]
                  [org.clojars.vladimirmarkovic86/xml-lib "0.1.1"]
                  ]
