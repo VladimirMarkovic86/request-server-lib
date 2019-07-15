b5ba247fc0ec56bb3dcb088f115659ab7173175f
VladimirMarkovic86 <markovic.vladimir86@gmail.com>
2019-07-15 18:00:28 +0200

    Task 88: Rolled back README.md and LICENSE files to appear in jar files.

--- project.clj
+++ project.clj
@@ -1,19 +1,16 @@
-(defproject org.clojars.vladimirmarkovic86/request-server-lib "0.1.8"
+(defproject org.clojars.vladimirmarkovic86/request-server-lib "0.1.9"
   :description "Request server library"
   :url "http://github.com/VladimirMarkovic86/request-server-lib"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
-                 [org.clojars.vladimirmarkovic86/utils-lib "0.4.12"]
-                 [org.clojars.vladimirmarkovic86/ajax-lib "0.1.13"]
-                 [org.clojars.vladimirmarkovic86/xml-lib "0.1.2"]
+                 [org.clojars.vladimirmarkovic86/utils-lib "0.4.13"]
+                 [org.clojars.vladimirmarkovic86/ajax-lib "0.1.14"]
+                 [org.clojars.vladimirmarkovic86/xml-lib "0.1.3"]
                  ]
 
   :min-lein-version "2.0.0"
   
   :source-paths ["src/clj"]
-  :test-paths ["test/clj"]
-  
-  :jar-exclusions [#"README.md$"
-                   #"LICENSE$"])
+  :test-paths ["test/clj"])
 
